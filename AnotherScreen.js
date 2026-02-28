64de9147e0f1f2ba4d8ad7c2b7a943932b581d77
James <jacobtarot.uk@gmail.com>
2026-02-28 17:13:51 +0100

    Add AnotherScreen.js component

---
+++ AnotherScreen.js
@@ -0,0 +1,13 @@
+// AnotherScreen.js
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const AnotherScreen = () => {
+    return (
+        <View>
+            <Text>This is another screen!</Text>
+        </View>
+    );
+};
+
+export default AnotherScreen;

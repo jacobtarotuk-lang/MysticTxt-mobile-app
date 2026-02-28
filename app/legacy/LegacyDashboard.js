396845b769ad1328e780d12fa3cbd49f40cd26df
James <jacobtarot.uk@gmail.com>
2026-02-28 18:04:09 +0100

    Add LegacyDashboard component

---
+++ app/legacy/LegacyDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const LegacyDashboard = () => {
+    return (
+        <View>
+            <Text>Welcome to the Legacy Dashboard!</Text>
+        </View>
+    );
+};
+
+export default LegacyDashboard;

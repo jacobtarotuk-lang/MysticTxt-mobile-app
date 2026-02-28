9ada5f9bc0f4edb8e58763e0a40de387aa3b9e5c
James <jacobtarot.uk@gmail.com>
2026-02-28 14:42:48 +0100

    Add orders list feature

---
+++ app/(legacy)/orders.js
@@ -0,0 +1,11 @@
+// Fetches and displays orders assigned to advisors
+
+const fetchOrders = async () => {
+  // Fetch logic here
+};
+
+const displayOrders = (orders) => {
+  // Display logic here
+};
+
+fetchOrders().then(displayOrders);

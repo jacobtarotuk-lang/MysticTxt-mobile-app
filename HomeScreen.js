852292d3975e19e9f9148f28600bcc76e908a007
James <jacobtarot.uk@gmail.com>
2026-02-28 17:13:13 +0100

    Add HomeScreen component

---
+++ HomeScreen.js
@@ -0,0 +1,13 @@
+// This is the HomeScreen component
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;

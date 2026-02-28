d244e1056bc2752cbece6201ccb4564ecc51261a
James <jacobtarot.uk@gmail.com>
2026-02-28 17:14:49 +0100

    Add LegacyHomeScreen.js with 'Legacy Portal Home' component

---
+++ LegacyHomeScreen.js
@@ -0,0 +1,14 @@
+// LegacyHomeScreen.js
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const LegacyHomeScreen = () => {
+    return (
+        <View>
+            <Text>Legacy Portal Home</Text>
+        </View>
+    );
+};
+
+export default LegacyHomeScreen;
